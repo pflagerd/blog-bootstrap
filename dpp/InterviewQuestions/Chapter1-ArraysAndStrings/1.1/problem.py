e315d576bb4cab65b3b8afc4b8fabfff072da643
Daniel P Pflager <daniel.pflager@gmail.com>
2026-01-26 09:20:35 -0800

    Refining problem statement.

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.1/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.1/problem.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-def isUnique(string):
+def isUnique():
     pass
     
 class IsUnique(unittest.TestCase):
@@ -9,13 +9,13 @@ class IsUnique(unittest.TestCase):
        self.assertEqual(isUnique(""), "Indeterminate")
 
     def test_2(self):
-       self.assertEqual(isUnique("a"), "True")
+       self.assertEqual(isUnique("a"), True)
 
     def test_3(self):
-       self.assertEqual(isUnique("ab"), "True")
+       self.assertEqual(isUnique("ab"), True)
 
     def test_4(self):
-       self.assertEqual(isUnique("aa"), "False")
+       self.assertEqual(isUnique("aa"), False)
 
     def test_5(self):
        self.assertEqual(isUnique("the quick brown fox jumps over the lazy dog"), "False")
