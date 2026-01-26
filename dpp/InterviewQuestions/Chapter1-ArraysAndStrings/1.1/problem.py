bd8c1bf3e5678d8e8ce175cefd0b838779459a7c
Daniel P Pflager <daniel.pflager@gmail.com>
2026-01-26 10:02:39 -0800

    Refining problem statement.

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.1/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.1/problem.py
@@ -1,12 +1,37 @@
 
 import unittest
 
-def isUnique():
-    pass
+def isASCII(string):
+    for c in string:
+        if ord(c) > 127:
+            return False
+
+    return True
+
+def isUnique(string):
+    if not len(string):
+        raise ValueError("string must not be empty")
+
+    if len(string) > 128: # There are 128 ASCII values. If the string is longer than 128, must be a duplicate character in it.
+        return False
+
+    if not isASCII(string):
+        raise ValueError("string must contain only ASCII characters.")
+
+    found = [False] * 128
+
+    for c in string:
+        if not found[ord(c)]:
+            found[ord(c)] = True
+        else:
+            return False
+
+    return True
     
 class IsUnique(unittest.TestCase):
     def test_1(self):
-       self.assertEqual(isUnique(""), "Indeterminate")
+       with self.assertRaises(ValueError):
+           isUnique("")
 
     def test_2(self):
        self.assertEqual(isUnique("a"), True)
@@ -18,7 +43,7 @@ class IsUnique(unittest.TestCase):
        self.assertEqual(isUnique("aa"), False)
 
     def test_5(self):
-       self.assertEqual(isUnique("the quick brown fox jumps over the lazy dog"), "False")
+       self.assertEqual(isUnique("the quick brown fox jumps over the lazy dog"), False)
 
 
 if __name__ == "__main__":
