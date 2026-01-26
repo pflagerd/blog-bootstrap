c4eeead39ef0ab750d070d503c1f6177e92cb88c
Daniel P Pflager <daniel.pflager@gmail.com>
2026-01-26 07:58:36 -0800

    progress

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.1/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.1/problem.py
@@ -1,18 +1,24 @@
 
 import unittest
 
-def isUnique(array, target):
+def isUnique(string):
     pass
     
 class IsUnique(unittest.TestCase):
     def test_1(self):
-       self.assertEqual(isUnique([-1, 1, 5, 7], 6), [])
+       self.assertEqual(isUnique(""), "Indeterminate")
 
     def test_2(self):
-       self.assertEqual(isUnique([1, 2], 3), [])
+       self.assertEqual(isUnique("a"), "True")
 
     def test_3(self):
-       self.assertEqual(isUnique([-1, 10, 12, 15], 125), [])
+       self.assertEqual(isUnique("ab"), "True")
+
+    def test_4(self):
+       self.assertEqual(isUnique("aa"), "False")
+
+    def test_5(self):
+       self.assertEqual(isUnique("the quick brown fox jumps over the lazy dog"), "False")
 
 
 if __name__ == "__main__":
