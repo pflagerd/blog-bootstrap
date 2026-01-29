1e1712bb386ffff6b4b0778ae43fe6b3ef9335b0
Daniel P Pflager <daniel.pflager@gmail.com>
2026-01-28 16:34:21 -0800

    Refining problem statement and test cases in problem.json. Generating files from it.

---
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.2/problem.py
@@ -0,0 +1,37 @@
+
+import unittest
+
+def checkPermutation(string1, string2):
+    pass
+    
+class CheckPermutation(unittest.TestCase):
+    def test_1(self):
+       self.assertEqual(checkPermutation("", ""), "Raise <code>ValueError</code>")
+
+    def test_2(self):
+       self.assertEqual(checkPermutation("", "abcd"), "Raise <code>ValueError</code>")
+
+    def test_3(self):
+       self.assertEqual(checkPermutation("ab", "bc"), "False")
+
+    def test_4(self):
+       self.assertEqual(checkPermutation("ab", "aba"), "False")
+
+    def test_5(self):
+       self.assertEqual(checkPermutation("ab", "ab"), "True")
+
+    def test_6(self):
+       self.assertEqual(checkPermutation("ab", "ba"), "True")
+
+    def test_7(self):
+       self.assertEqual(checkPermutation("The quick brown fox jumps over the lazy dog", "The quick brown fox jumps over the lazy dog"), "True")
+
+    def test_8(self):
+       self.assertEqual(checkPermutation("The quick brown fox jumps over the lazy dog", "the quick brown fox jumps over the lazy dog"), "False")
+
+    def test_9(self):
+       self.assertEqual(checkPermutation("The quick brown fox springs over the lazy dog", "the quick brown fox jumps over the lazy dog"), "False")
+
+
+if __name__ == "__main__":
+    unittest.main()
