ab53e68a54d9cfe515c435fca62af346d8cfc91b
Daniel P Pflager <daniel.pflager@gmail.com>
2026-02-04 09:44:05 -0800

    Solution uses dictionary to hold characterCount.

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.2/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.2/problem.py
@@ -1,6 +1,27 @@
 
 import unittest
 
+def checkPermutationASCIIOnly(string1, string2):
+    pass
+
+def createCharacterCount(string1):
+    characterCount = {}
+    for character in string1:
+        ordCharacter = ord(character)
+        if ordCharacter not in characterCount:
+            characterCount[ordCharacter] = 1
+        else:
+            characterCount[ordCharacter] += 1
+    return characterCount
+
+
+def checkPermutationUnicode(string1, string2):
+    string1CharacterCount = createCharacterCount(string1)
+    string2CharacterCount = createCharacterCount(string2)
+
+    return string1CharacterCount == string2CharacterCount
+
+
 def checkPermutation(string1, string2):
     if string1 is None or string2 is None:
         raise ValueError("Neither string1 nor string2 can be None")
@@ -8,8 +29,9 @@ def checkPermutation(string1, string2):
     if not string1 or not string2:
         raise ValueError("Neither string1 nor string2 can be empty")
 
-    pass
-    
+    #return checkPermutationASCIIOnly(string1, string2)
+    return checkPermutationUnicode(string1, string2)
+
 class CheckPermutation(unittest.TestCase):
     def test_1(self):
         with self.assertRaises(ValueError):
@@ -45,14 +67,16 @@ class CheckPermutation(unittest.TestCase):
        self.assertEqual(checkPermutation("ab", "ba"), True)
 
     def test_7(self):
-       self.assertEqual(checkPermutation("The quick brown fox jumps over the lazy dog", "The quick brown fox jumps over the lazy dog"), "True")
+       self.assertEqual(checkPermutation("The quick brown fox jumps over the lazy dog", "The quick brown fox jumps over the lazy dog"), True)
 
     def test_8(self):
-       self.assertEqual(checkPermutation("The quick brown fox jumps over the lazy dog", "the quick brown fox jumps over the lazy dog"), "False")
+       self.assertEqual(checkPermutation("The quick brown fox jumps over the lazy dog", "the quick brown fox jumps over the lazy dog"), False)
 
     def test_9(self):
-       self.assertEqual(checkPermutation("The quick brown fox springs over the lazy dog", "the quick brown fox jumps over the lazy dog"), "False")
+       self.assertEqual(checkPermutation("The quick brown fox springs over the lazy dog", "the quick brown fox jumps over the lazy dog"), False)
 
+    def test_10(self):
+       self.assertEqual(checkPermutation("⚬ӎĪ⎐╿⛑☲∑Ӿ〈ü☍γ⌲⛃ϲ↾↑⌥ҤÐ≷Ý≊{čř⊦≘┸⊨☧⛶Ξi⎕♸⊖⇺⊫⁖ś⎲☧©♴⚻☗♺⇇⍦⍄≚∴Ӕ⌂ҋ⎸♓⌐⏃┲Αʹ", "Ξ⎕⌥⊨♴∑ü⁖⇺≷γ⚻⎐©Ý♺┲≚☍┸♸⌂ř⌲⌐↾⊫⛶♓〈č☧╿∴↑≊≘☗☧ҤӔ⊦ĪҋΑ⇇Ð{⏃śi⛑⎸⛃ӎ⍄⎲ʹӾ⊖ϲ⚬⍦☲"), True)
 
 if __name__ == "__main__":
     unittest.main()
