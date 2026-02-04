2d96ab84b1f2059f4ce3f9db30e6f04e1c9c6dfa
Daniel P Pflager <daniel.pflager@gmail.com>
2026-02-04 08:46:43 -0800

    Add beef to first couple tests.

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.2/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.2/problem.py
@@ -2,26 +2,47 @@
 import unittest
 
 def checkPermutation(string1, string2):
+    if string1 is None or string2 is None:
+        raise ValueError("Neither string1 nor string2 can be None")
+
+    if not string1 or not string2:
+        raise ValueError("Neither string1 nor string2 can be empty")
+
     pass
     
 class CheckPermutation(unittest.TestCase):
     def test_1(self):
-       self.assertEqual(checkPermutation("", ""), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            checkPermutation(None, "abc")
+
+        with self.assertRaises(ValueError):
+            checkPermutation("abc", None)
+
+        with self.assertRaises(ValueError):
+            checkPermutation(None, None)
 
     def test_2(self):
-       self.assertEqual(checkPermutation("", "abcd"), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            checkPermutation("", "abc")
+
+        with self.assertRaises(ValueError):
+            checkPermutation("abc", "")
+
+        with self.assertRaises(ValueError):
+            checkPermutation("", "")
+
 
     def test_3(self):
-       self.assertEqual(checkPermutation("ab", "bc"), "False")
+       self.assertEqual(checkPermutation("ab", "bc"), False)
 
     def test_4(self):
-       self.assertEqual(checkPermutation("ab", "aba"), "False")
+       self.assertEqual(checkPermutation("ab", "aba"), False)
 
     def test_5(self):
-       self.assertEqual(checkPermutation("ab", "ab"), "True")
+       self.assertEqual(checkPermutation("ab", "ab"), True)
 
     def test_6(self):
-       self.assertEqual(checkPermutation("ab", "ba"), "True")
+       self.assertEqual(checkPermutation("ab", "ba"), True)
 
     def test_7(self):
        self.assertEqual(checkPermutation("The quick brown fox jumps over the lazy dog", "The quick brown fox jumps over the lazy dog"), "True")
