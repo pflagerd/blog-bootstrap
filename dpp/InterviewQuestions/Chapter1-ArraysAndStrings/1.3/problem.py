1c1dd1d0ebe2fdeb01b4e37ddf6f18ca6184cbaf
Daniel P Pflager <daniel.pflager@gmail.com>
2026-02-16 08:28:49 -0800

    Make tests actually catch assertions.

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.3/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.3/problem.py
@@ -1,39 +1,49 @@
-
 import unittest
+
 
 def urlify(string, length):
     pass
-    
+
+
 class Urlify(unittest.TestCase):
     def test_1(self):
-       self.assertEqual(urlify("Mr John Smith    ", "13"), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            urlify("Mr John Smith    ", "13")
 
     def test_2(self):
-       self.assertEqual(urlify("None", "13"), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            urlify("None", "13")
 
     def test_3(self):
-       self.assertEqual(urlify("Mr John Smith    ", "None"), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            urlify("Mr John Smith    ", "None")
 
     def test_4(self):
-       self.assertEqual(urlify("None", "None"), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            urlify("None", "None")
 
     def test_5(self):
-       self.assertEqual(urlify("", "13"), "")
+        self.assertEqual(urlify("", "13"), "")
 
     def test_6(self):
-       self.assertEqual(urlify("Mr John Smith    ", ""), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            urlify("Mr John Smith    ", "")
 
     def test_7(self):
-       self.assertEqual(urlify("Mr John Smith    ", "-1"), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            urlify("Mr John Smith    ", "-1")
 
     def test_8(self):
-       self.assertEqual(urlify("Mr John Smith    ", "3"), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            urlify("Mr John Smith    ", "3")
 
     def test_9(self):
-       self.assertEqual(urlify("Mr John Smith    ", "12"), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            urlify("Mr John Smith    ", "12")
 
     def test_10(self):
-       self.assertEqual(urlify("Mr John Smith    ", "14"), "Raise <code>ValueError</code>")
+        with self.assertRaises(ValueError):
+            urlify("Mr John Smith    ", "14")
 
 
 if __name__ == "__main__":
