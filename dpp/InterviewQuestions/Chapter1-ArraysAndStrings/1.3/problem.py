99dde34a1cbb9ef3c818203b94d4781a8f657e73
Daniel P Pflager <daniel.pflager@gmail.com>
2026-02-16 09:32:25 -0800

    All but 4 cases pass.

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.3/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.3/problem.py
@@ -1,29 +1,60 @@
 import unittest
+from tkinter.constants import LEFT
 
 
-def urlify(string, length):
-    pass
+def urlify(string: str, length: int) -> str:
+    if string is None or not isinstance(string, str) or not string or length is None or not isinstance(length, int) or length < 0:
+        raise ValueError("string and length must be non-Non,  non-negative and of type str and int respectively")
+
+    if length == 0:
+        return string
+
+    # We're requested to do it in place, so I'll need to use a list
+    # Work backwards, Use two pointers. Starting with length - 1'th character copy character by character, replacing spaces with %20, end when we've encountered (len(string) - length)/2 spaces
+    stringAsList = []
+    for char in string:
+        stringAsList.append(char)
+
+    left = length - 1
+    right = len(stringAsList) - 1
+    nSpaces = (len(stringAsList) - length) // 2
+    i = 0
+    while i < nSpaces:
+        c = stringAsList[left]
+        if c == ' ':
+            stringAsList[right] = '0'
+            right -= 1
+            stringAsList[right] = '2'
+            right -= 1
+            stringAsList[right] = '%'
+            right -= 1
+            i += 1
+        else:
+            stringAsList[right] = stringAsList[left]
+            right -= 1
+        left -= 1
+
+    return "".join(stringAsList)
 
 
 class Urlify(unittest.TestCase):
     def test_1(self):
-        with self.assertRaises(ValueError):
-            urlify("Mr John Smith    ", "13")
+        self.assertEqual("Mr%20John%20Smith", urlify("Mr John Smith    ", 13))
 
     def test_2(self):
         with self.assertRaises(ValueError):
-            urlify("None", "13")
+            urlify(None, 13)
 
     def test_3(self):
         with self.assertRaises(ValueError):
-            urlify("Mr John Smith    ", "None")
+            urlify("Mr John Smith    ", None)
 
     def test_4(self):
         with self.assertRaises(ValueError):
-            urlify("None", "None")
+            urlify(None, None)
 
     def test_5(self):
-        self.assertEqual(urlify("", "13"), "")
+        self.assertEqual(urlify("", 13), "")
 
     def test_6(self):
         with self.assertRaises(ValueError):
@@ -31,19 +62,24 @@ class Urlify(unittest.TestCase):
 
     def test_7(self):
         with self.assertRaises(ValueError):
-            urlify("Mr John Smith    ", "-1")
+            urlify("Mr John Smith    ", -1)
 
     def test_8(self):
-        with self.assertRaises(ValueError):
-            urlify("Mr John Smith    ", "3")
+        self.assertEqual("Mr%20John Smith   ", urlify("Mr John Smith    ", 3))
 
     def test_9(self):
-        with self.assertRaises(ValueError):
-            urlify("Mr John Smith    ", "12")
+        self.assertEqual("Mr%20John%20Smith", urlify("Mr John Smith    ", 12))
 
     def test_10(self):
         with self.assertRaises(ValueError):
-            urlify("Mr John Smith    ", "14")
+            urlify("Mr John Smith    ", 14)
+
+    def test_11(self):
+        self.assertEqual("Mr John Smith    ", urlify("Mr John Smith    ", 0))
+
+    def test_12(self):
+        with self.assertRaises(ValueError):
+            urlify("", 0)
 
 
 if __name__ == "__main__":
