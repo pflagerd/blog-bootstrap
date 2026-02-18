c866cce6ed9669c940a2b85aac17741f33c7d647
Daniel P Pflager <daniel.pflager@gmail.com>
2026-02-18 08:53:40 -0800

    Just keep working on stuff.

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.3/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.3/problem.py
@@ -36,22 +36,58 @@ def urlifyBackwards(string: str, length: int) -> str:
 
     return "".join(stringAsList)
 
+#
+# Gayle's solution, converted to python from Java.  Error checks added by Daniel
+#
+def urlify(s, true_length):
+    if s is None or not isinstance(s, str) or not s or true_length is None or not isinstance(true_length, int) or true_length < 0:
+        raise ValueError("string and length must be non-None, non-negative and of type str and int respectively")
 
-def urlify(string: str, length: int) -> str:
+    space_count = 0
+    for i in range(true_length):
+        if s[i] == ' ':
+            space_count += 1
+
+    index = true_length + space_count * 2
+    s = list(s)
+    if true_length < len(s):
+        s[true_length] = '\0'
+
+    for i in range(true_length - 1, -1, -1):
+        if s[i] == ' ':
+            s[index - 1] = '0'
+            s[index - 2] = '2'
+            s[index - 3] = '%'
+            index -= 3
+        else:
+            s[index - 1] = s[i]
+            index -= 1
+
+    return ''.join(s)
+
+
+def urlifyInWork(string: str, length: int) -> str:
     if string is None or not isinstance(string, str) or not string or length is None or not isinstance(length, int) or length < 0:
         raise ValueError("string and length must be non-None, non-negative and of type str and int respectively")
 
     # We're requested to do it in place, so I'll need to use a list
     # Work backwards, Use two pointers. Starting with length - 1'th character copy character by character, replacing spaces with %20, end when we've encountered (len(string) - length)/2 spaces
     originalLength = len(string)
-    stringAsList = []
+    stringAsList = list(string)
+    i = 0
     for char in string:
         if length != 0:
             if char == ' ':
-                stringAsList.extend(list('%20'))
+                stringAsList[i] = '%'
+                i += 1
+                stringAsList[i] = '2'
+                i += 1
+                stringAsList[i] = '0'
+                i += 1
             else:
-                stringAsList.append(char)
-            if len(stringAsList) > originalLength:
+                stringAsList[i] = char
+                i += 1
+            if i > originalLength:
                 raise ValueError("Overran the string length")
             length -= 1
 
