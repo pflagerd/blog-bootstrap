6bb46d6b38cc86b54b6ef6015db5eb771a9f58fe
Daniel P Pflager <daniel.pflager@gmail.com>
2026-03-23 10:01:14 -0700

    Progress on 1.5

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.5/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.5/problem.py
@@ -1,9 +1,31 @@
 
 import unittest
 
+statement = "There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away. ",
+
+# insert a character
+# remove a character
+# replace a character (remove a character followed by a insert a character at the same position)
+
+
 def oneAway(string):
-    pass
-    
+    diff = [0] * 26
+
+    strings = string.split(", ")
+
+    for c in strings[0]:
+        diff[ord(c) - ord('a')] += 1
+
+    print(diff)
+
+    for c in strings[1]:
+        diff[ord(c) - ord('a')] -= 1
+
+    print(diff)
+
+    print(sum(diff))
+
+
 class OneAway(unittest.TestCase):
     def test_1(self):
        self.assertEqual(oneAway("pale, ple"), "True")
