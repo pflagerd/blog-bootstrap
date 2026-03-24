df6590d31d0d6c50ce8ba84aa7217b055fc7dcc5
Daniel P Pflager <daniel.pflager@gmail.com>
2026-03-24 08:51:52 -0700

    progress

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.5/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.5/problem.py
@@ -7,6 +7,10 @@ statement = "There are three types of edits that can be performed on strings: in
 # remove a character
 # replace a character (remove a character followed by a insert a character at the same position)
 
+# If different lengths, can have one more character on left than right, or converse. (i.e. one character different between them)
+# I the same length, must have exactly oe character different between them
+
+
 
 def oneAway(string):
     diff = [0] * 26
@@ -28,16 +32,22 @@ def oneAway(string):
 
 class OneAway(unittest.TestCase):
     def test_1(self):
-       self.assertEqual(oneAway("pale, ple"), "True")
+        print("pale, ple")
+        self.assertEqual(oneAway("pale, ple"), "True")
+
+-
 
     def test_2(self):
-       self.assertEqual(oneAway("pales, pale"), "True")
+        print("pales, pale")
+        self.assertEqual(oneAway("pales, pale"), "True")
 
     def test_3(self):
-       self.assertEqual(oneAway("pale, bale"), "True")
+        print("pale, bale")
+        self.assertEqual(oneAway("pale, bale"), "True")
 
     def test_4(self):
-       self.assertEqual(oneAway("pale, bake"), "False")
+        print("pale, bake")
+        self.assertEqual(oneAway("pale, bake"), "False")
 
 
 if __name__ == "__main__":
