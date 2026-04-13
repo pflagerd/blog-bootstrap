b7b5eb2e11c985b346e4af06f4a31cfb98d6642c
Daniel P Pflager <daniel.pflager@gmail.com>
2026-04-13 09:54:39 -0700

    Progress.

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.6/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.6/problem.py
@@ -2,14 +2,86 @@
 import unittest
 
 def stringCompression(string):
-    pass
-    
+    return compressStringA(string)
+
+# finite state machine
+def compressStringA(string):
+    EOT = chr(4)
+
+    characterCount = 0
+    outputString = ""
+    originalString = string
+    previousCharacter = chr(0)
+
+    def initialState(character):
+        nonlocal previousCharacter
+        nonlocal characterCount
+        nonlocal currentState
+
+        if character == EOT:
+            finalState(character)
+
+        previousCharacter = character
+        characterCount = 1
+        currentState = foundCurrentCharacter
+
+    def finalState(character):
+        if len(outputString) < len(originalString):
+            return outputString
+        else:
+            return originalString
+
+    def foundCurrentCharacter(character):
+        nonlocal previousCharacter
+        nonlocal characterCount
+        nonlocal currentState
+        nonlocal originalString
+        nonlocal outputString
+
+        if character == EOT:
+            outputString += str(previousCharacter) + str(characterCount)
+            currentState = finalState
+            if len(outputString) < len(originalString):
+                return outputString
+            else:
+                return originalString
+
+        if character == previousCharacter:
+            characterCount += 1
+        else:
+            outputString += str(previousCharacter) + str(characterCount)
+            characterCount = 1
+            previousCharacter = character
+
+
+    currentState = initialState
+
+    for character in string:
+        currentState(character)
+    return currentState(EOT)
+
+
+
+
+def finiteStateAutomaton(string):
+    EOT = chr(4)
+
+    def initialState(character):
+        pass
+
+    currentState = initialState
+
+    for character in string:
+        currentState(character)
+    currentState(EOT)
+
+
 class StringCompression(unittest.TestCase):
     def test_1(self):
-       self.assertEqual(stringCompression("aabcccccaaa"), "a2blc5a3")
+       self.assertEqual( "a2b1c5a3", stringCompression("aabcccccaaa"))
 
     def test_2(self):
-       self.assertEqual(stringCompression("abcdef"), "abcdef")
+       self.assertEqual("abcdef", stringCompression("abcdef"))
 
 
 if __name__ == "__main__":
