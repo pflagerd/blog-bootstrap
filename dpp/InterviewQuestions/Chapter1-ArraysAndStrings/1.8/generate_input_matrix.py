e20f41e7b45d4570bcb1b5a6f9981e6ffd3e13f4
Daniel P Pflager <daniel.pflager@gmail.com>
2026-05-11 10:25:44 -0700

    Title, problem statement and hints

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.8/generate_input_matrix.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.8/generate_input_matrix.py
@@ -1,11 +1,11 @@
-def generate_square_matrix(n: int) -> list[list[int]]:
-    return [[n * row + col + 1 for col in range(n)] for row in range(n)]
+def generate_input_matrix(M: int, N: int) -> list[list[int]]:
+    return [[N * row + col + 1 for col in range(N)] for row in range(M)]
 
 
 # Example usage
 if __name__ == "__main__":
-    for size in [2, 3, 4, 10]:
-        matrix = generate_square_matrix(size)
+    for size in [[2, 3], [3, 1], [4, 7], [10, 10]]:
+        matrix = generate_input_matrix(size[0], size[1])
         row_number = 1
         print("[", end="")
         for row in matrix:
--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.8/problem.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.8/problem.py
@@ -1,24 +1,35 @@
 
 import unittest
 
-def rotateMatrix(matrix):
-    pass
+def zeroMatrix(matrix):
+    rows_to_zero = [0] * len(matrix)
+    cols_to_zero = [0] * len(matrix[0])
+    for row in matrix:
+        for col in row:
+            if matrix[row][col] == 0:
+                rows_to_zero[row] = cols_to_zero[col] = 1
+    for row in matrix:
+
+    for col in row:
+            if matrix[row][col] == 0:
+                rows_to_zero[row] = cols_to_zero[col] = 1
+
     
-class RotateMatrix(unittest.TestCase):
+class ZeroMatrix(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(None, rotateMatrix(None))
+        self.assertEqual([[0, 0, 0], [0, 5, 6]], zeroMatrix([[0, 2, 3], [4, 5, 6]]))
 
-    def test_2(self):
-        self.assertEqual(None, rotateMatrix([]))
-
-    def test_3(self):
-        self.assertEqual(None, rotateMatrix([[]]))
-
-    def test_4(self):
-        self.assertEqual(None, rotateMatrix([[1]]))
-
-    def test_5(self):
-       self.assertEqual(None, rotateMatrix([[1, 2]]))
+    # def test_2(self):
+    #     self.assertEqual(None, zeroMatrix([]))
+    #
+    # def test_3(self):
+    #     self.assertEqual(None, zeroMatrix([[]]))
+    #
+    # def test_4(self):
+    #     self.assertEqual(None, zeroMatrix([[1]]))
+    #
+    # def test_5(self):
+    #    self.assertEqual(None, zeroMatrix([[1, 2]]))
 
 
 if __name__ == "__main__":
