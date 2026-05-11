b8a1a7d98a7f61ee34dc3e4226adca95ab9fc12d
Daniel P Pflager <daniel.pflager@gmail.com>
2026-05-11 16:39:18 -0700

    rename

--- dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.8/generate_input_matrices.py
+++ dpp/InterviewQuestions/Chapter1-ArraysAndStrings/1.8/generate_input_matrices.py
@@ -1,11 +1,11 @@
-def generate_input_matrix(M: int, N: int) -> list[list[int]]:
+def generate_input_matrices(M: int, N: int) -> list[list[int]]:
     return [[N * row + col + 1 for col in range(N)] for row in range(M)]
 
 
 # Example usage
 if __name__ == "__main__":
     for size in [[2, 3], [3, 1], [4, 7], [10, 10]]:
-        matrix = generate_input_matrix(size[0], size[1])
+        matrix = generate_input_matrices(size[0], size[1])
         row_number = 1
         print("[", end="")
         for row in matrix:
