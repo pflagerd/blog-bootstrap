4a27a1bdc92a9246f3cfde6621dc09315101c8dc
Daniel P Pflager <daniel.pflager@gmail.com>
2026-06-24 08:03:44 -0700

    progress

--- dpp/InterviewQuestions/Chapter02-LinkedLists/2.2/problem.py
+++ dpp/InterviewQuestions/Chapter02-LinkedLists/2.2/problem.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import json
 import unittest
 
 
@@ -14,7 +15,7 @@ class SinglyLinkedListNode:
 
     # <code>n</code> is passed the number of nodes to be created in the Singly Linked List. Must be greater than zero.
     @staticmethod
-    def generate(n: int) -> SinglyLinkedListNode | None:
+    def generate_X(n: int) -> SinglyLinkedListNode | None:
 
         if False:
 
@@ -57,33 +58,7 @@ class SinglyLinkedListNode:
                 sll.next = SinglyLinkedListNode(i)
 
 
-            i = 0
-            head = SinglyLinkedListNode(i)
-
-            i += 1
-            head.next = SinglyLinkedListNode(i)
-
-            i += 1
-            head.next.next = SinglyLinkedListNode(i)
-
-            i += 1
-            head.next.next.next = SinglyLinkedListNode(i)
-
-
 ######
-            i = 0
-            head = sll = SinglyLinkedListNode(i)
-
-            i += 1
-            sll.next = SinglyLinkedListNode(i)
-            sll = sll.next
-
-            i += 1
-            sll.next = SinglyLinkedListNode(i)
-            sll = sll.next
-
-            i += 1
-            sll.next = SinglyLinkedListNode(i)
 
 
 
@@ -155,6 +130,259 @@ class SinglyLinkedListNode:
         return head
 
 
+    #
+    # David proposed unrolling the loop
+    # We conjectured that there is always a beginning, middle and end to a loop
+    # We therefore thought it wise to have at least 3 iterations in the unrolled loop, one for beginning, one for middle and one for end.
+    # We positted that having 4 or 5 might make the choice of which one is the middle more clear, and settled on 4 for this situation.
+    # We reckoned this was the most concise representation for 4 items
+    #
+    # Where this approach becomes awkward is in the lines like:
+    #   <code>head.next.next.next = SinglyLinkedListNode(i)</code>
+    # because <code>head.next.next...next</code> could get ridiculously long
+    #
+    # So we decided to contract that syntax, which led us to <code>generate_B</code>
+    #
+    @staticmethod
+    def generate_A(n: int):
+        if n != 4: # this is here to make it clear that this produces exactly 4 <code>SinglyLinkedListNode</code>s
+            return None
+
+        head = SinglyLinkedListNode(0)
+
+        head.next = SinglyLinkedListNode(1)
+
+        head.next.next = SinglyLinkedListNode(2)
+
+        head.next.next.next = SinglyLinkedListNode(3)
+
+        return head
+
+    #
+    # David proposed unrolling the loop
+    # We conjectured that there is always a beginning, middle and end to a loop
+    # We therefore thought it wise to have at least 3 iterations in the unrolled loop, one for beginning, one for middle and one for end.
+    # We positted that having 4 or 5 might make the choice of which one is the middle more clear, and settled on 4 for this situation.
+    # We reckoned this was the most concise representation for 4 items
+    #
+    # Where this approach becomes awkward is in the lines like:
+    #   <code>head.next.next.next = SinglyLinkedListNode(i)</code>
+    # because <code>head.next.next...next</code> could get ridiculously long
+    #
+    # So we decided to contract that syntax, which led us to <code>generate_B</code>
+    #
+    @staticmethod
+    def generate_B(n: int):
+        if n != 4: # this is here to make it clear that this produces exactly 4 <code>SinglyLinkedListNode</code>s
+            return None
+
+        i = 0
+        head = SinglyLinkedListNode(i)
+
+        i += 1
+        head.next = SinglyLinkedListNode(i)
+
+        i += 1
+        head.next.next = SinglyLinkedListNode(i)
+
+        i += 1
+        head.next.next.next = SinglyLinkedListNode(i)
+
+        return head
+
+    #
+    # We learned that compound assignments like:
+    #   <code>head = sll = SinglyLinkedListNode(i)</code>
+    # are evaluated in Python differently than other programming languages and our intuition
+    #
+    # For example the code above is equivalent to something like this:
+    #   <code>x = SinglyLinkedListNode(i)</code>
+    #   <code>head = x</code>
+    #   <code>sll = x</code>
+    #
+    # We noticed that we could group lines of code together arbitrarily to
+    # define beginning, middle and end, with the notion that repeated code
+    # in the middle would become our looped code.
+    #
+    # Applying the grouping shown below led us to <code>generate_C</code>
+    #
+    @staticmethod
+    def generate_C(n: int):
+        # What is this code block and ones like it (intended to inoculate against bad input data) called?
+        if n <= 0:
+            return None
+
+        # beginning
+        i = 0
+        head = sll = SinglyLinkedListNode(i)
+
+        # middle
+        i += 1
+        sll.next = SinglyLinkedListNode(i)
+        sll = sll.next
+
+        i += 1
+        sll.next = SinglyLinkedListNode(i)
+        sll = sll.next
+
+        # end
+        i += 1
+        sll.next = SinglyLinkedListNode(i)
+
+        return head
+
+    #
+    # Having identified the beginning, middle and end, we focus on the middle to craft our loop.
+    # Proceeding woodenly as if an automaton, I find the repeated block of code in the middle
+    # eliminate repetition and indent it under a <code>while True:</code>. This is as if there were a <code>Loop:</code> statement or something.
+    # Next decision is where and how to exit the loop.
+    # We could put the loop before or after any of the lines of code of the repeated middle, so why not start at the beginning?
+    # We place the exit condition (<code>if [expression]:</code>) and a <code>break</code> at the beginning.
+    # How do we choose what <code><i>[expression]</i></code> will be?
+    #
+    # We know we're dealing with a count of nodes (<code>n</code>), so we realize we need to compare <code>i</code> with <code>n</code>:
+    #
+    # So in Python, we have the following choices:
+    #   <code>i < n</code>
+    #   <code>i == n</code>
+    #   <code>i <= n</code>
+    #   <code>i > n</code>
+    #   <code>i >= n</code>
+    #   <code>i != n</code>
+    #
+    # The one that leaped to our minds was:
+    #   <code>i >= n</code>
+    # We felt this was the right answer because we knew there would be one node created before exit condition was reached in the code,
+    # so for the case <code>n == 1</code> we wanted the exit condition to leave the loop immediately.
+    #   <code>if i == 1:<code>
+    #   <code>  break</code>
+    #
+    # But we fell into a trap, because we concluded prematurely that we were done!
+    # We forgot that there was an "end" block of code.  So the following code is subtly broken for the case where n == 1.
+    # In such a case, it will create two nodes rather than one, because both the beginning and the end will always be executed.
+    #
+    @staticmethod
+    def generate_D(n: int):
+        # What is this code block and ones like it (intended to inoculate against bad input data) called?
+        if n <= 0:
+            return None
+
+        # beginning
+        ### This will always be called ###
+        i = 0
+        head = sll = SinglyLinkedListNode(i)
+
+        # middle
+        while True:
+            if i >= n:
+                break
+            i += 1
+            sll.next = SinglyLinkedListNode(i)
+            sll = sll.next
+
+        # end
+        ### This will ALSO always be called ###
+        i += 1
+        sll.next = SinglyLinkedListNode(i)
+
+        return head
+
+    #
+    # So we realized that if we insist on having three sections (beginning, middle, and end)
+    # then perhaps we are going to have to ensure that some or all three sections have (exit) conditions to prevent their unwanted execution?
+    @staticmethod
+    def generate_E(n: int):
+        # What is this code block and ones like it (intended to inoculate against bad input data) called?
+        if n <= 0:
+            return None
+
+        # beginning
+        ### This will always be called ###
+        i = 0
+        head = sll = SinglyLinkedListNode(i)
+
+        # middle
+        while True:
+            i += 1
+            if i >= n:
+                return head
+            sll.next = SinglyLinkedListNode(i)
+            sll = sll.next
+
+        # end
+        ### This will now NEVER be called ###
+        i += 1
+        sll.next = SinglyLinkedListNode(i)
+
+        return head
+
+
+
+
+
+
+
+
+    # if in first loop do something special
+    #
+    # So we realized that if we insist on having three sections (beginning, middle, and end)
+    # then perhaps we are going to have to ensure that some or all three sections have (exit) conditions to prevent their unwanted execution?
+    @staticmethod
+    def generate_F(n: int):
+        # What is this code block and ones like it (intended to inoculate against bad input data) called?
+        if n <= 0:
+            return None
+
+        # beginning
+        ### This will always be called ###
+        i = 0
+
+        # middle
+        while True:
+            head = sll = SinglyLinkedListNode(i)
+            i += 1
+            if i >= n:
+                return head
+            sll.next = SinglyLinkedListNode(i)
+            sll = sll.next
+
+        # end
+        ### This will ALSO always be called ###
+        i += 1
+        sll.next = SinglyLinkedListNode(i)
+
+        return head
+
+    #
+    # So we realized that if we insist on having three sections (beginning, middle, and end)
+    # then perhaps we are going to have to ensure that some or all three sections have (exit) conditions to prevent their unwanted execution?
+    @staticmethod
+    def generate_F(n: int):
+        # What is this code block and ones like it (intended to inoculate against bad input data) called?
+        if n <= 0:
+            return None
+
+        # beginning
+        ### This will always be called ###
+        i = 0
+        head = sll = SinglyLinkedListNode(i)
+
+        # middle
+        while True:
+            i += 1
+            sll.next = SinglyLinkedListNode(i)
+            if i > n:
+                break # return?
+            sll = sll.next
+
+        # end
+        ### This will ALSO always be called ###
+        i += 1
+        sll.next = SinglyLinkedListNode(i)
+
+        return head
+
+
 
 
 # return integer offset between 0 and len(s1) if s2 is a substring of s1, else return -1
@@ -165,10 +393,49 @@ def returnKthToLastA(head: SinglyLinkedListNode | None, k: int) -> int | None:
 returnKthToLast = returnKthToLastA
     
 class ReturnKthToLastTests(unittest.TestCase):
-    def test_0(self):
-        self.assertEqual(None, SinglyLinkedListNode.generate(2)) # Degenerate test case. Null singly linked list.
+    def test_lang_00(self): # DISSONANCE!  Python will often (but not always) optimize string literals to be the same object (reference)
+        self.assertIs('{"value": 4, "next": null}', '{"value": 4, "next": null}')
+
+    def test_lang_00s(self): # AMBIGUOUS!  Can't gell if python is comparing references or values
+        self.assertTrue('{"value": 4, "next": null}' == '{"value": 4, "next": null}')
+
+    def test_lang_01(self):
+        self.assertIsNot('{"value": 4, "next": null}',  json.dumps(SinglyLinkedListNode(4).__dict__))
+
+    def test_lang_02(self):
+        self.assertEqual('{"value": 4, "next": null}', json.dumps(SinglyLinkedListNode(4).__dict__))
+
+    def test_lang_03(self): # == compares CONTENTS of strings, not references
+        self.assertTrue('{"value": 4, "next": null}' == json.dumps(SinglyLinkedListNode(4).__dict__))
 
 
+    def test_lang_11(self):
+        x = SinglyLinkedListNode(4)
+        self.assertIs(x, x) # Language test case. Does python compare references?
+        self.assertEqual(x, x) # AMBIGUOUS: Language test case. Does python compare references?
+        self.assertTrue(x == x) # AMBIGUOUS: Language test case. Does python compare references?
+        # Yes, it compares references for objects
+
+    def test_lang_12(self):
+        self.assertNotEqual(SinglyLinkedListNode(4), SinglyLinkedListNode(4))  # Language test case. Does python compare references?
+        # Yes, it compares references
+
+
+    # def test_0(self):
+    #     self.assertEqual(json.dumps(SinglyLinkedListNode(0).generate_A(0).__dict__), json.dumps(SinglyLinkedListNode(0).generate_A(0).__dict__))
+
+
+
+
+
+    # def test_1(self):
+    #     self.assertEqual(SinglyLinkedListNode(1), SinglyLinkedListNode.generate(1)) # Degenerate test case. Null singly linked list.
+    #
+    # def test_2(self):
+    #     self.assertEqual(None, SinglyLinkedListNode.generate(2)) # Degenerate test case. Null singly linked list.
+    #
+    # def test_3(self):
+    #     self.assertEqual(None, SinglyLinkedListNode.generate(3)) # Degenerate test case. Null singly linked list.
 
     # def test_1(self):
     #     self.assertEqual(None, returnKthToLast(None, 0)) # Degenerate test case. Null singly linked list.
