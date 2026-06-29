b71ea16d3d1a3d7131c327c1170fd4ede41667b1
Daniel P Pflager <daniel.pflager@gmail.com>
2026-06-29 09:46:48 -0700

    progress

--- dpp/InterviewQuestions/Chapter02-LinkedLists/2.2/problem.py
+++ dpp/InterviewQuestions/Chapter02-LinkedLists/2.2/problem.py
@@ -13,133 +13,34 @@ class SinglyLinkedListNode:
         self.next = None
         self.value = value
 
+    def __eq__(self, other):
+        if not isinstance(other, SinglyLinkedListNode):
+            return False
+
+        current_self = self.next
+        current_other = other.next
+
+        # Traverse both lists simultaneously
+        while current_self and current_other:
+            if current_self.value != current_other.value:
+                return False
+            current_self = current_self.next
+            current_other = current_other.next
+
+        # If both are None, they are the same length and identical
+        return current_self is None and current_other is None
+
     def dumps(self):
         s = ""
-        s = "{\"value\": " + str(self.value)
-        s += ", next: "
+        s += "{next: "
         if self.next is None:
             s += "null"
         else:
             s += str(self.next.dumps())
+        s += ", \"value\": " + str(self.value)
         s += "}"
         return s
 
-
-    # <code>n</code> is passed the number of nodes to be created in the Singly Linked List. Must be greater than zero.
-    @staticmethod
-    def generate_X(n: int) -> SinglyLinkedListNode | None:
-
-        if False:
-
-            if n <= 0:
-                return None
-
-            sll = SinglyLinkedListNode(0) # <code>sll</code> stands for Singly Linked List. Temporary variable for loop support.
-            head = sll # points to the head of the list
-            for i in range(1, n):
-                sll.next = SinglyLinkedListNode(i)
-                sll = sll.next
-
-
-            i = 1
-            while i < n:
-                sll.next = SinglyLinkedListNode(i)
-                sll = sll.next
-                i += 1
-
-            i = 1
-            while True:
-                if i >= n:
-                    break
-                sll.next = SinglyLinkedListNode(i)
-                sll = sll.next
-                i += 1
-
-            i = 0
-            while True:
-                sll.next = SinglyLinkedListNode(i)
-                if i == 0:
-                    head = sll
-                sll = sll.next
-
-
-                if i >= n:
-                    break
-
-                i += 1
-                sll.next = SinglyLinkedListNode(i)
-
-
-######
-
-
-
-            i = 0
-
-            head = sll = SinglyLinkedListNode(i)
-            i += 1
-
-            sll.next = SinglyLinkedListNode(i)
-            sll = sll.next
-            i += 1
-
-            sll.next = SinglyLinkedListNode(i)
-            sll = sll.next
-            i += 1
-
-            sll.next = SinglyLinkedListNode(i)
-
-
-
-
-            i = 0
-
-            head = sll = SinglyLinkedListNode(i)
-            i += 1
-            sll.next = SinglyLinkedListNode(i)
-
-            sll = sll.next
-            i += 1
-            sll.next = SinglyLinkedListNode(i)
-
-            sll = sll.next
-            i += 1
-            sll.next = SinglyLinkedListNode(i)
-
-        i = 0
-
-        while True:
-            if i == 0:
-                head = sll = SinglyLinkedListNode(i)
-            else:
-                sll.next = SinglyLinkedListNode(i)
-                sll = sll.next
-            if i == n - 1:
-                break
-            i += 1
-
-        # i = 0
-        #
-        # head = sll = SinglyLinkedListNode(i)
-        #
-        # # i += 1
-        # # sll.next = tail = sll = SinglyLinkedListNode(i)
-        #
-        # i += 1
-        # tail = sll = sll.next = SinglyLinkedListNode(i)
-
-
-
-        # tail = sll.next = sll = SinglyLinkedListNode(i)
-        #
-        # x = SinglyLinkedListNode(i)
-        # tail = x
-        # sll.next = x
-        # sll = x
-
-
-
-        return head
 
     #
     # David proposed unrolling the loop
@@ -170,17 +71,20 @@ class SinglyLinkedListNode:
         return head
 
     #
-    # David proposed unrolling the loop
-    # We conjectured that there is always a beginning, middle and end to a loop
-    # We therefore thought it wise to have at least 3 iterations in the unrolled loop, one for beginning, one for middle and one for end.
-    # We positted that having 4 or 5 might make the choice of which one is the middle more clear, and settled on 4 for this situation.
-    # We reckoned this was the most concise representation for 4 items
+    # We decide to think about parameterizing things, and factoring out the common
+    # code, reasoning the beginning might look a little different from the middle
+    # which will look a little different from the end.
     #
-    # Where this approach becomes awkward is in the lines like:
-    #   <code>head.next.next.next = SinglyLinkedListNode(i)</code>
-    # because <code>head.next.next...next</code> could get ridiculously long
+    # <code>i</code> is the most obvious choice, and since <code>i = 0</code> has to happen before we
+    # pass <code>i</code> to the <code>head.next... = SinglyLinkedListNode(i)</code> lines, there
+    # seems to emerge groups of pairs of lines like:
     #
-    # So we decided to contract that syntax, which led us to <code>generate_B</code>
+    # <code>i <i>= something</i></code>
+    # <code>head<i>.something </i>= SinglyLinkedListNode(i)</code>
+    #
+    # So we could group lines of code together arbitrarily to
+    # define beginning, middle and end, with the notion that repeated code
+    # (in the middle?) would become our looped code.
     #
     @staticmethod
     def generate_B(n: int):
@@ -202,20 +106,24 @@ class SinglyLinkedListNode:
         return head
 
     #
+    # If we're going to do something about the <code>head.next...</code> syntactic phenomenon, we're
+    # going to need to keep track of <code>head</code> and be able to append new nodes to one another.
+    #
+    # This sort of sounds like a <code>tail</code> 
+    #
     # We learned that compound assignments like:
-    #   <code>head = sll = SinglyLinkedListNode(i)</code>
-    # are evaluated in Python differently than other programming languages and our intuition
+    #   <code>head = tail = SinglyLinkedListNode(i)</code>
+    # are evaluated in Python differently than other programming languages.
     #
-    # For example the code above is equivalent to something like this:
-    #   <code>x = SinglyLinkedListNode(i)</code>
+    # Our intuition was based on math and on other C-like programming languages.
+    #
+    # Python evaluates the compound assignment above as something like this:
+    #   <code>x = SinglyLinkedListNode(i) # x is an "invisible" temporary variable.</code>
     #   <code>head = x</code>
-    #   <code>sll = x</code>
+    #   <code>tail = x</code>
     #
-    # We noticed that we could group lines of code together arbitrarily to
-    # define beginning, middle and end, with the notion that repeated code
-    # in the middle would become our looped code.
-    #
-    # Applying the grouping shown below led us to <code>generate_C</code>
+    # Now some differences in the pattern of the groupings made a beginning, middle and end emerge.
+    # They are shown as comments in the code below:
     #
     @staticmethod
     def generate_C(n: int):
@@ -225,20 +133,20 @@ class SinglyLinkedListNode:
 
         # beginning
         i = 0
-        head = sll = SinglyLinkedListNode(i)
+        head = tail = SinglyLinkedListNode(i)
 
         # middle
         i += 1
-        sll.next = SinglyLinkedListNode(i)
-        sll = sll.next
+        tail.next = SinglyLinkedListNode(i)
+        tail = tail.next
 
         i += 1
-        sll.next = SinglyLinkedListNode(i)
-        sll = sll.next
+        tail.next = SinglyLinkedListNode(i)
+        tail = tail.next
 
         # end
         i += 1
-        sll.next = SinglyLinkedListNode(i)
+        tail.next = SinglyLinkedListNode(i)
 
         return head
 
@@ -281,20 +189,20 @@ class SinglyLinkedListNode:
         # beginning
         ### This will always be called ###
         i = 0
-        head = sll = SinglyLinkedListNode(i)
+        head = tail = SinglyLinkedListNode(i)
 
         # middle
         while True:
             if i >= n:
                 break
             i += 1
-            sll.next = SinglyLinkedListNode(i)
-            sll = sll.next
+            tail.next = SinglyLinkedListNode(i)
+            tail = tail.next
 
         # end
         ### This will ALSO always be called ###
         i += 1
-        sll.next = SinglyLinkedListNode(i)
+        tail.next = SinglyLinkedListNode(i)
 
         return head
 
@@ -310,20 +218,50 @@ class SinglyLinkedListNode:
         # beginning
         ### This will always be called ###
         i = 0
-        head = sll = SinglyLinkedListNode(i)
+        head = tail = SinglyLinkedListNode(i)
+
+        # middle
+        while True:
+            if i > n - 1: # CHANGED to make it get called one time less.
+                break
+            i += 1
+            tail.next = SinglyLinkedListNode(i)
+            tail = tail.next
+
+        # end
+        ### This will ALSO always be called ###
+        i += 1
+        tail.next = SinglyLinkedListNode(i)
+
+        return head
+
+
+    #
+    # So we realized that if we insist on having three sections (beginning, middle, and end)
+    # then perhaps we are going to have to ensure that some or all three sections have (exit) conditions to prevent their unwanted execution?
+    @staticmethod
+    def generate_F(n: int):
+        # What is this code block and ones like it (intended to inoculate against bad input data) called?
+        if n <= 0:
+            return None
+
+        # beginning
+        ### This will always be called ###
+        i = 0
+        head = tail = SinglyLinkedListNode(i)
 
         # middle
         while True:
             i += 1
             if i >= n:
                 return head
-            sll.next = SinglyLinkedListNode(i)
-            sll = sll.next
+            tail.next = SinglyLinkedListNode(i)
+            tail = tail.next
 
         # end
         ### This will now NEVER be called ###
         i += 1
-        sll.next = SinglyLinkedListNode(i)
+        tail.next = SinglyLinkedListNode(i)
 
         return head
 
@@ -416,14 +354,17 @@ class ReturnKthToLastTests(unittest.TestCase):
         self.assertTrue('{"value": 4, "next": null}' == '{"value": 4, "next": null}')
 
 
+    # The __dict__ method actually orders the keys in the order they were defined (typically in the __init__)
+    # Since we defined next first, the following should fail.
     def test_lang_01(self):
         self.assertIsNot('{"value": 4, "next": null}',  json.dumps(SinglyLinkedListNode(4).__dict__))
 
+    # But this one should pass, because the order matches.
     def test_lang_02(self):
-        self.assertEqual('{"value": 4, "next": null}', json.dumps(SinglyLinkedListNode(4).__dict__))
+        self.assertEqual('{"next": null, "value": 4}', json.dumps(SinglyLinkedListNode(4).__dict__))
 
-    def test_lang_03(self): # == compares CONTENTS of strings, not references
-        self.assertTrue('{"value": 4, "next": null}' == json.dumps(SinglyLinkedListNode(4).__dict__))
+    def test_lang_03(self): # The == operator compares CONTENTS of strings, not references
+        self.assertTrue('{"next": null, "value": 4}' == json.dumps(SinglyLinkedListNode(4).__dict__))
 
 
     def test_lang_11(self):
@@ -433,28 +374,45 @@ class ReturnKthToLastTests(unittest.TestCase):
         self.assertTrue(x == x) # AMBIGUOUS: Language test case. Does python compare references?
         # Yes, it compares references for objects
 
+    # Except now we defined an __eq__ method so contents rather than references are compared
     def test_lang_12(self):
-        self.assertNotEqual(SinglyLinkedListNode(4), SinglyLinkedListNode(4))  # Language test case. Does python compare references?
-        # Yes, it compares references
-
-        # class Dummy:
-        #     pass
-        #
-        # print(Dummy.__dict__)
-        #print(json.dumps(SinglyLinkedListNode.__init__()))
-        # print(json.dumps(SinglyLinkedListNode(0).__dict__))
+        self.assertEqual(SinglyLinkedListNode(4), SinglyLinkedListNode(4))
 
 
+    # Test __dict__.  This works because json.dumps() displays the keys in the order the attributes (.next and .value) were defined (in __init__)
     def test_20(self):
-        self.assertEqual('{"value": 0, "next": null}', json.dumps(SinglyLinkedListNode(0).__dict__))
+        self.assertEqual('{"next": null, "value": 0}', json.dumps(SinglyLinkedListNode(0).__dict__))
 
-        # sll = SinglyLinkedListNode(0)
-        # sll.generate = sll.generate_X
-        # print(sll.generate(1))
+    def test_21(self):
+        self.assertNotEqual('{"value": 0, "next": null}', json.dumps(SinglyLinkedListNode(0).__dict__))
 
-    def test_100(self):
+
+    # Notice that the way we've created the dumps() method, the keys appear in alphabetic order, so the following doesn't work
+    # even though according to the json standard it should be allowed because json allows keys to appear in any order.
+    # Since we're using literal strings, and because self.assertX() compares literal strings literally, we've got this
+    # minor conundrum.
+    def test_generate_A_00(self):
+        self.assertNotEqual('{"value": 0, "next": null}', json.dumps(SinglyLinkedListNode(0).dumps()))
+
+    def test_generate_A_10(self):
         print(SinglyLinkedListNode.generate_A(4).dumps())
-        #self.assertEqual('{value: 0, next: {value: 1, next: {value: 2, next: {value: 3, next: null}}}}', json.dumps(SinglyLinkedListNode.generate_A.__dict__))
+        self.assertEqual('{next: {next: {next: {next: null, "value": 3}, "value": 2}, "value": 1}, "value": 0}', SinglyLinkedListNode.generate_A(4).dumps())
+
+
+    def test_generate_B_00(self):
+        self.assertEqual(SinglyLinkedListNode.generate_A(4), SinglyLinkedListNode.generate_B(4))
+
+
+    def test_generate_C_00(self):
+        self.assertEqual(SinglyLinkedListNode.generate_A(4), SinglyLinkedListNode.generate_C(4))
+
+
+    def test_generate_D_00(self):
+        self.assertNotEqual(SinglyLinkedListNode.generate_A(4), SinglyLinkedListNode.generate_D(4))
+
+
+    def test_generate_E_00(self):
+        self.assertEqual(SinglyLinkedListNode.generate_A(4), SinglyLinkedListNode.generate_E(4))
 
 
     # def test_100(self):
