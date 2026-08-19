5156a4e425ba3d799b6142880f97ad86208885e2
Daniel P Pflager <daniel.pflager@gmail.com>
2026-08-19 16:41:35 -0700

    rename from filename(s) to problem(s).

--- setup_next_problem.py
+++ setup_next_problem.py
@@ -7,7 +7,9 @@ verbose = False
 def main(args):
     global verbose
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="set up dpp/InterviewQuestions/... based on the given problem number(s). e.g. 2.4 sets up dpp/InterviewQuestions/Chapter02-LinkedLists/2.4 with files and packages so that tests and algorithm code can be written in python there."
+    )
     parser.add_argument("-v", "--verbose", action="store_true")
     parser.add_argument("problems", nargs="*")
     parsed = parser.parse_args(args[1:])
