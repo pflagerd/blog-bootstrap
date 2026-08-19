e88ac1ef055acc9ed7f659ad22e2f3d760a12459
Daniel P Pflager <daniel.pflager@gmail.com>
2026-08-19 16:08:55 -0700

    Very small template with filename arguments.

--- setup_next_problem.py
+++ setup_next_problem.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
+import argparse
 import sys
 
+verbose = False
+
 def main(args):
+    global verbose
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("filenames", nargs="*")
+    parsed = parser.parse_args(args[1:])
+    verbose = parsed.verbose
+
+    for filename in parsed.filenames:
+        print(filename)
+
     return 0
 
 if __name__ == "__main__":
